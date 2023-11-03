b444775671ec2db9725de60efd12a1b3217af5dd
holywater0826_zempot <holywater0826@zempot.com>
2023-11-03 11:53:40 +0900

    init

---
+++ gRPC/README.md
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+- abseil
+https://github.com/abseil/abseil-cpp
+
+git clone https://github.com/abseil/abseil-cpp.git
+cd abseil-cpp
+cmake .
+
+- grpc
+https://github.com/grpc/grpc
+
+
+git clone https://github.com/grpc/grpc.git
+cd grpc
+git submodule update --init
+cmake .
+
+
+- protobuf
+https://github.com/protocolbuffers/protobuf
+
+
+- gRPC에 protobuf, abseil 라이브러리 필요함
+
+protoc -I=E:\ProtobufTest --grpc_out=.\ --cpp_out=.\ --csharp_out=.\ --plugin=protoc-gen-grpc=grpc_cpp_plugin.exe TestProtobuf.proto
+
+-I : 프로토 파일 경로
+--cpp_out : cpp 출력 경로
+--csharp_out : cs 출력 경로
+--grpc_out : grpc 출력 경로
+--plugin : grpc를 함께 컴파일하려면, 플러그인 옵션을 추가한다.
+TestProtobuf.proto : 프로토파일명
---
+++ gRPC/gRPCServer/gRPCServer.cpp
@@ -0,0 +1,47 @@
+﻿// gRPCServer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+#include <grpc/grpc.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+
+#include "pingpong.grpc.pb.h"
+#include "pingpong.pb.h"
+
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerWriter;
+using grpc::Status;
+
+using dy::PingPongService;
+using dy::Ping;
+using dy::Pong;
+
+class Listernr final : public PingPongService::Service {
+    Status pingpong(ServerContext* context, const Ping*, Pong* pong)  {
+
+        return Status::OK;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    Listernr service;
+    std::string address("0.0.0.0:8888");
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "server listening on " << address << std::endl;
+    server->Wait();
+
+}
